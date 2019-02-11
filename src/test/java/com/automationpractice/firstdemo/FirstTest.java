6ab68ecd67c7fece8612e1f9526240326131cdda
world2enjoy <47141001+world2enjoy@users.noreply.github.com>
2019-02-11 13:01:56 +0530

    Update FirstTest.java

--- src/test/java/com/automationpractice/firstdemo/FirstTest.java
+++ src/test/java/com/automationpractice/firstdemo/FirstTest.java
@@ -31,7 +31,7 @@ public class FirstTest {
 	public void firstTest() throws InterruptedException {
 
 		// Optional, if not specified, WebDriver will search your path for driver exe.
-		System.setProperty("webdriver.gecko.driver", "C:/Users/Nick/WebDriver/Gecko/geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "chromedriver.exe");
 		
 		// Create a new Chrome WebDriver (browser) object and maximize the browser window (optional)
 		driver = new FirefoxDriver();
