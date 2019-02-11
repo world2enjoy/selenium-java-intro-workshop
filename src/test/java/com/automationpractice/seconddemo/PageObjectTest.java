61f45d17fe9342f354834115740f40e922c0e39f
world2enjoy <47141001+world2enjoy@users.noreply.github.com>
2019-02-11 13:07:44 +0530

    Update PageObjectTest.java

--- src/test/java/com/automationpractice/seconddemo/PageObjectTest.java
+++ src/test/java/com/automationpractice/seconddemo/PageObjectTest.java
@@ -23,7 +23,7 @@ public class PageObjectTest {
 	public void testNumberofDressesAvailable() {
 
 		// Optional, if not specified, WebDriver will search your path for chromedriver.
-		System.setProperty("webdriver.gecko.driver", "chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "chromedriver");
 
 		// Create a new Chrome WebDriver (browser) object
 		driver = new FirefoxDriver();
